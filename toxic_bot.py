411c42f9f389ceada5aa0908e71c9df2f291e774
aticie <efehanatici@gmail.com>
2020-02-09 22:50:23 +0300

    Recent play links to profile

--- toxic_bot.py
+++ toxic_bot.py
@@ -79,7 +79,8 @@ async def recent(ctx, *args):
 
     embed = discord.Embed(title=title_text, description=desc_text, color=0x00ff00, url=bmap_url)
     embed.set_image(url="attachment://recent.png")
-    embed.set_author(name=f"Most recent play of {osu_username}", icon_url=f"https://a.ppy.sh/{player_id}?.png")
+    embed.set_author(name=f"Most recent play of {osu_username}", url=f"https://osu.ppy.sh/users/{player_id}",
+                     icon_url=f"https://a.ppy.sh/{player_id}?.png")
 
     await ctx.send(embed=embed, file=discord.File('recent.png'))
 
